9bf8282d8a176a06fcb206ee2ea30eb65f537ece
kmkm001 <kmkmdev@gmail.com>
2015-09-16 14:44:06 +0900

    Add feature of settings storage.

---
+++ FilerForDev/DataFile.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace FilerForDev
+{
+    class DataFile
+    {
+        #region 定義
+        /*-------------------------------------------------------------------------
+		*	定義
+		*------------------------------------------------------------------------*/
+
+        #endregion 定義
+
+        #region メンバ
+        /*-------------------------------------------------------------------------
+		*	メンバ
+		*------------------------------------------------------------------------*/
+
+        #endregion メンバ
+
+        #region メソッド
+        /*-------------------------------------------------------------------------
+		*	メソッド
+		*------------------------------------------------------------------------*/
+        /// <summary>
+        /// 保存
+        /// </summary>
+        /// <typeparam name="TypeName"></typeparam>
+        /// <param name="data"></param>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        protected Boolean LoadFromXMLFile<TypeName>(ref TypeName data, string path)
+        {
+            Boolean ret = true;
+            // XmlSerializerオブジェクトを作成
+            XmlSerializer serializer = new XmlSerializer(typeof(TypeName));
+            // 読み込むファイルを開く
+            StreamReader sr = new StreamReader(path, new UTF8Encoding(false));
+            try
+            {
+                // XMLファイルから読み込み、逆シリアル化する
+                data = (TypeName)serializer.Deserialize(sr);
+            }
+            catch (Exception e)
+            {
+                ret = false;
+            }
+            finally
+            {
+                // ファイルを閉じる
+                sr.Close();
+            }
+            return ret;
+        }
+
+        /// <summary>
+        /// 読み込み
+        /// </summary>
+        /// <typeparam name="TypeName"></typeparam>
+        /// <param name="data"></param>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        protected Boolean SaveToXMLFile<TypeName>(TypeName data, string path)
+        {
+            Boolean ret = true;
+            // XmlSerializerオブジェクトを作成
+            //オブジェクトの型を指定する
+            XmlSerializer serializer = new XmlSerializer(typeof(TypeName));
+            // 書き込むファイルを開く（UTF-8 BOM無し）
+            StreamWriter sw = new StreamWriter(path, false, new UTF8Encoding(false));
+            try
+            {
+                // シリアル化し、XMLファイルに保存する
+                serializer.Serialize(sw, data);
+            }
+            catch (Exception e)
+            {
+                ret = false;
+            }
+            finally
+            {
+                // ファイルを閉じる
+                sw.Close();
+            }
+            return ret;
+        }
+
+        #endregion メソッド
+
+    }
+}
---
+++ FilerForDev/Settings.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections;
+
+namespace FilerForDev
+{
+	// 本クラスの機能
+	//   汎用的な設定データの管理
+	//   ファイル読み込み、書き込み(ファイル形式は未定)
+
+    public class Settings : DataFile
+	{
+
+		#region 定義
+		/*-------------------------------------------------------------------------
+		*	定義
+		*------------------------------------------------------------------------*/
+
+		#endregion 定義
+
+		#region メンバ
+		/*-------------------------------------------------------------------------
+		*	プロパティ
+		*------------------------------------------------------------------------*/
+		[System.Xml.Serialization.XmlIgnore]
+		// 設定ファイル
+		private string _settingsFileName = @"SettingsFile.xml";
+        public string SettingsFile
+        {
+            get { return _settingsFileName; }
+            set { _settingsFileName = value; }
+        }
+		// 起動時の表示設定
+		[System.Xml.Serialization.XmlElement("DefaultVisible")]
+		private bool _defaultVisible = true;
+        public bool DefaultVisible
+        {
+            get { return _defaultVisible; }
+            set { _defaultVisible = value; }
+        }
+        // ソートモードのインデックス
+		[System.Xml.Serialization.XmlElement("SortModeIndex")]
+		private int _sortModeIndex = 0;
+        public int SortModeIndex
+        {
+            get { return _sortModeIndex; }
+            set { _sortModeIndex = value; }
+        }
+
+		#endregion メンバ
+
+		#region メソッド
+		/*-------------------------------------------------------------------------
+		*	メソッド
+		*------------------------------------------------------------------------*/
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
+		/// <param name="fileName"></param>
+		public Settings(String fileName){
+			_settingsFileName = fileName;
+		}
+
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
+        public Settings()
+		{
+		}
+
+		/// <summary>
+		/// 読み込み
+		/// </summary>
+		/// <returns></returns>
+		public Boolean LoadSettings(){
+			Boolean ret = false;
+            Settings data = new Settings();
+			try
+			{
+				ret = LoadFromXMLFile(ref data, _settingsFileName);
+			}
+			catch
+			{
+				return ret;
+			}
+            // --------------------------------------------------
+            // 読み込む設定
+			this._defaultVisible = data._defaultVisible;
+			this._sortModeIndex = data._sortModeIndex;
+            // --------------------------------------------------
+
+			data = null;
+			return ret;
+		}
+
+		/// <summary>
+		/// 保存
+		/// </summary>
+		/// <returns></returns>
+		public Boolean SaveSettings()
+		{
+			Boolean ret = false;
+			try
+			{
+				ret = SaveToXMLFile(this, _settingsFileName);
+			}
+			catch
+			{
+				return ret;
+			}
+			return ret;
+		}
+
+		#endregion メソッド
+
+	}
+}
